e74dbccfec16a4e7cc39efa89b0a7aa1be4c4969
Charan89781 <154489542+Charan89781@users.noreply.github.com>
2026-08-01 14:39:21 +0530

    Add files via upload

---
+++ LAB EXPERIMENTS/EXPERIMENT 8/EXP 08 CODE.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[100];
+    printf("Enter binary string: ");
+    scanf("%s", str);
+    if(strstr(str, "101") != NULL)
+        printf("String Accepted\n");
+    else
+        printf("String Rejected\n");
+    return 0;
+}
